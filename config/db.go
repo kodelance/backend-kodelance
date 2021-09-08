0e575488b3794b4b98d244644bbfe69f922d56a8
kobulwidodo <fadhilhan01@gmail.com>
2021-09-08 15:51:26 +0700

    Change mysql to postgresql

--- config/db.go
+++ config/db.go
@@ -1,21 +1,25 @@
 package config
 
 import (
+	"fmt"
 	"kodelance/user"
 	"log"
 	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDb() (*gorm.DB, error) {
-	db_host := os.Getenv("DB_HOST")
-	db_name := os.Getenv("DB_NAME")
-	db_pass := os.Getenv("DB_PASS")
-	db_user := os.Getenv("DB_USER")
-	dsn := db_user + ":" + db_pass + "@tcp(" + db_host + ")/" + db_name + "?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := db_user + ":" + db_pass + "@tcp(" + db_host + ")/" + db_name + "?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+		"5432",
+	)
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
